71180043db635fb0ef924ebfc39620a0476cb122
Janne Fagerström <jannematiasf@gmail.com>
2021-03-05 09:16:55 +0200

    deploy

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,7 +2,7 @@ let navOpen: boolean = false;
 
 function toggleHamburger() {
   navOpen = !navOpen;
-  console.log("navOpen", navOpen);
+  // console.log("navOpen", navOpen);
   const navbar = document.getElementById("nav");
   navbar?.classList.toggle("hide-nav");
 }
