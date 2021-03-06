afbabe56874697a0c76ee98d31e71e2e9c0d4bd6
Janne Fagerström <jannematiasf@gmail.com>
2021-03-06 15:26:09 +0200

    email copy

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,8 +1,24 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let navOpen = false;
 function toggleHamburger() {
     navOpen = !navOpen;
-    console.log("navOpen", navOpen);
+    // console.log("navOpen", navOpen);
     const navbar = document.getElementById("nav");
     navbar === null || navbar === void 0 ? void 0 : navbar.classList.toggle("hide-nav");
+}
+function copyEmail() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let emailText = "jannematiasf@gmail.com";
+        navigator.clipboard.writeText(emailText);
+        alert("Copied to clipboard: " + emailText);
+    });
 }
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -6,3 +6,10 @@ function toggleHamburger() {
   const navbar = document.getElementById("nav");
   navbar?.classList.toggle("hide-nav");
 }
+
+async function copyEmail() {
+  let emailText: string = "jannematiasf@gmail.com";
+  navigator.clipboard.writeText(emailText);
+
+  alert("Copied to clipboard: " + emailText);
+}
