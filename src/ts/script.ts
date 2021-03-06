daf3c2a4e882635531a8ebbbfbc8d2a126682d7c
Janne Fagerström <jannematiasf@gmail.com>
2021-03-06 17:30:39 +0200

    navbar toggle

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,15 +1,21 @@
 let navOpen: boolean = false;
 
-function toggleHamburger() {
+function toggleHamburgerMain() {
   navOpen = !navOpen;
-
-  const header = document.getElementById("header");
-
   const navbar = document.getElementById("nav");
   navbar?.classList.toggle("hide-nav");
 }
 
-async function copyEmail() {
+function toggleHamburger() {
+  if (850 < window.innerWidth) {
+    return;
+  }
+  navOpen = !navOpen;
+  const navbar = document.getElementById("nav");
+  navbar?.classList.toggle("hide-nav");
+}
+
+async function copyEmail(): Promise<void> {
   let emailText: string = "jannematiasf@gmail.com";
   navigator.clipboard.writeText(emailText);
 
